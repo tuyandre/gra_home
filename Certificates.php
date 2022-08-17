73f55cac19bba75fcdbc096eff58139cde694b47
tuyandre <36557797+tuyandre@users.noreply.github.com>
2022-08-17 06:17:47 +0200

    INITIAL GRA

---
+++ Certificates.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Secular+One&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/download.css">
+  <link rel="stylesheet" type="text/css"
+    href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://fontawesome.com/icons/a?s=solid">
+  <title>CIS & VSDC Technical Specifications</title>
+</head>
+<style>
+  .evat-download {
+    display: grid;
+    grid-template-columns: 5fr 5fr;
+    margin-top: 6%;
+    text-align: center;
+  }
+
+  .evat-download a {
+    color: #e4d01c;
+    padding-top: 2%;
+  }
+  .certs{
+  display: grid;
+  grid-template-columns: 5fr 5fr;
+}
+  .pro-one input {
+    background-color: #c2a813;
+    color: white;
+  }
+  .cert-class{
+     padding-bottom: 4%;
+  }
+  .btn{
+      height: 9vh;
+      width: 12vw;
+  }
+</style>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light my-nav">
+    <img src="./ASSETS/GRA11.png" alt="" width="100">
+    <button class="navbar-toggler" type="button" id="top-icon" data-toggle="collapse" data-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse src-nav" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+      </ul>
+
+    </div>
+  </nav>
+
+
+  <!-- ===============================================  middle of the body ========================================================== -->
+
+  <div class="second-body container">
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+    <div class="b-content">
+      <div>
+        <div class="cert-class text-center">
+        <h3 class="text-center">
+        CERTIFIED INVOICING SYSTEM FOR E-Vat 
+        <h3>and </h3> 
+        </h3>
+        <h3>VDSC  TECHNICAL SPECIFICATIONS</h3>
+        </div>
+        <div class="jdk-download certs col text-center">
+          
+          <div class="pro-one">
+            <a href="https://gra.evatgra.com/Specification-for-CIS.html" target="_blank">
+              <button class="btn btn-warning bg-info">VCIS</button>
+              
+            </a>
+          </div>
+            <a href="https://gra.evatgra.com/VSDC_info.php" target="_blank">
+              <button class="btn btn-warning bg-success" >VSDC</button>
+              
+            </a>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+
+  </div>
+</body>
+
+<!-- =================================================   footer   ============================================================ -->
+
+
+<footer class=" bg_color text-center text-white">
+  <!-- Grid container -->
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+
+  <!-- Copyright -->
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
---
+++ VSDC_info.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>VSDC</title>
+
+</head>
+
+<body>
+ 
+    <iframe src="./BWE/Package/20180328_vsdc_technical_specifications_1.pdf" width="100%" height="700px">
+
+    </iframe>
+  </body>
+
+</html>
